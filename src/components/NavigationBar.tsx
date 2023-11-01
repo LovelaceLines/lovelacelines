6a04f89c7359d26e6c37b05ecab8bd426c7998e4
George Maia <georgemaiaf@gmail.com>
2023-10-31 22:01:54 -0300

    feat: Navigation Bar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,15 +1,35 @@
-import { AppBar, Box, Container, SvgIcon, Toolbar } from '@mui/material';
+import { AppBar, Box, Button, Container, IconButton, Link, SvgIcon, Toolbar } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import Brightness5Icon from '@mui/icons-material/Brightness5';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
 import logo from '../assets/logo.svg';
 import logoAlternative from '../assets/logo.alternative.svg';
+import { useTheme } from '../shared/themes';
 
 
 export const NavigationBar = () => {
+  const { themeName, toggleTheme } = useTheme();
+
   return (
-    <AppBar position="static">
+    <AppBar style={{height:'56px'}}>
       <Container>
         <Toolbar>
           <Box>
-            <img src={logo} alt='Logo' style={{width:'100%', height:'100%'}}/>
+            <img src={logo} alt='Logo' style={{height:'24px'}}/>
+          </Box>
+          <Box>
+            <Link>Inicio</Link>
+            <Link>Soluções</Link>
+            <Link>Desenvolvimento</Link>
+            <Link>Contatos</Link>
+          </Box>
+          <Box>
+            <IconButton onClick={toggleTheme}>
+              {themeName === 'light' ? <Brightness4Icon /> : <Brightness5Icon />}
+            </IconButton>
+            <IconButton><LinkedInIcon /></IconButton>
+            <IconButton><GitHubIcon /></IconButton>
           </Box>
         </Toolbar>
       </Container>
